3f0fa07cc9016ab6492e19e1248f33f751006d3f
itxMUmerFarooqBajwa321 <umerfarooqbajwa321@gmail.com>
2025-08-16 14:11:42 +0500

    minor change in button behaviour in absence of text in input field

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    document.querySelector('#task_form').onsubmit = ()=>{
-        let newTask = document.querySelector('#task').value ;
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelector('#submit').disabled = true;
+    
+    document.querySelector('#task').onchange = () => {       //onke
+        document.querySelector('#submit').disabled = false;
+    };
+    
+    document.querySelector('#task_form').onsubmit = () => {
+        let newTask = document.querySelector('#task').value;
 
-        const li =document.createElement('li');
+        const li = document.createElement('li');
         li.innerHTML = newTask;
+        
         document.querySelector('#tasks').append(li);
+        document.querySelector('#task').value = '';
+        
+        document.querySelector('#submit').disabled = true;
+
         return false;
     };
 });
